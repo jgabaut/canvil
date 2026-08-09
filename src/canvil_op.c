3ab0c3355c77edbff1f305c22f31833535fbd9df
jgabaut <109908086+jgabaut@users.noreply.github.com>
2026-08-09 12:42:20 +0000

    feat: pass anvil_version to op_build(), op_init()

--- src/canvil_op.c
+++ src/canvil_op.c
@@ -342,7 +342,7 @@ bool canvil_op_delete(const char* targetdir_optarg, const char* tagname, const c
     return res;
 }
 
-bool canvil_op_build(bool git_mode, bool force, bool no_rebuild, bool use_config_arg, const char* config_optarg, const char* minmake_optarg, const char* minautomake_version, const char* cflags_optarg, const char* targetdir_optarg, const char* builds_dir_optarg, const char* tagname, const char* bin_optarg, const char* source_optarg, const char* kern, AnvilPy_Env anvilpy_env, const char* custom_builder, char** extra_args, size_t extra_args_len, Spuro logger, Koliseo* kls)
+bool canvil_op_build(bool git_mode, bool force, bool no_rebuild, bool use_config_arg, const char* config_optarg, const char* minmake_optarg, const char* minautomake_version, const char* cflags_optarg, const char* targetdir_optarg, const char* builds_dir_optarg, const char* tagname, const char* bin_optarg, const char* source_optarg, const char* kern, const char* anvil_version_optarg, AnvilPy_Env anvilpy_env, const char* custom_builder, char** extra_args, size_t extra_args_len, Spuro logger, Koliseo* kls)
 {
     assert(kls != NULL);
 
@@ -518,7 +518,7 @@ bool canvil_op_purge(const char* targetdir_optarg, Canvil_Tag_List tag_list, con
     return res;
 }
 
-bool canvil_op_init(bool git_mode, bool force, bool no_rebuild, bool use_config_arg, const char* config_optarg, const char* minmake_optarg, const char* minautomake_version, const char* cflags_optarg, const char* targetdir_optarg, Canvil_Tag_List tag_list, const char* builds_dir_optarg, const char* bin_optarg, const char* source_optarg, const char* kern, AnvilPy_Env anvilpy_env, const char* custom_builder, char** extra_args, size_t extra_args_len, Spuro logger, Koliseo* kls)
+bool canvil_op_init(bool git_mode, bool force, bool no_rebuild, bool use_config_arg, const char* config_optarg, const char* minmake_optarg, const char* minautomake_version, const char* cflags_optarg, const char* targetdir_optarg, Canvil_Tag_List tag_list, const char* builds_dir_optarg, const char* bin_optarg, const char* source_optarg, const char* kern, const char* anvil_version_optarg, AnvilPy_Env anvilpy_env, const char* custom_builder, char** extra_args, size_t extra_args_len, Spuro logger, Koliseo* kls)
 {
     assert(kls != NULL);
     spr_logf_to(logger, SPR_INFO, "Doing init for {%s}, bin is {%s}", targetdir_optarg, bin_optarg);
@@ -531,7 +531,7 @@ bool canvil_op_init(bool git_mode, bool force, bool no_rebuild, bool use_config_
         char tag_repr[200] = {0};
         snprintf(tag_repr, 200, SemVer_Fmt, SemVer_Arg(node_value));
         tag_repr[199] = '\0';
-        bool build_res = canvil_op_build(git_mode, force, no_rebuild, use_config_arg, config_optarg, minmake_optarg, minautomake_version, cflags_optarg, targetdir_optarg, builds_dir_optarg, tag_repr, bin_optarg, source_optarg, kern, anvilpy_env, custom_builder, extra_args, extra_args_len, logger, kls);
+        bool build_res = canvil_op_build(git_mode, force, no_rebuild, use_config_arg, config_optarg, minmake_optarg, minautomake_version, cflags_optarg, targetdir_optarg, builds_dir_optarg, tag_repr, bin_optarg, source_optarg, kern, anvil_version_optarg, anvilpy_env, custom_builder, extra_args, extra_args_len, logger, kls);
         if (build_res) {
             spr_logtf_to(logger, SPR_INFO, "Success building {%s/v%s/%s}", targetdir_optarg, tag_repr, bin_optarg);
             successes++;
