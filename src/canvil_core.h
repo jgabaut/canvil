8af53108916bc3f45bd6f9570b7485e5594eb0c5
jgabaut <109908086+jgabaut@users.noreply.github.com>
2026-08-12 14:54:30 +0000

    chore: support for 2.1.4

--- src/canvil_core.h
+++ src/canvil_core.h
@@ -22,7 +22,7 @@
 #define CANVIL_MINOR 1
 #define CANVIL_PATCH 4
 
-#define EXPECTED_AMBOSO_API_LEVEL "2.1.3"
+#define EXPECTED_AMBOSO_API_LEVEL "2.1.4"
 
 #ifndef _WIN32
 #include <regex.h>
@@ -49,7 +49,7 @@ extern const SemVer MIN_AMBOSO_V_KERN;
 extern const SemVer MIN_AMBOSO_V_TREEGEN;
 extern const SemVer MIN_AMBOSO_V_ANVILPY_KERN;
 extern const SemVer MIN_AMBOSO_V_REFUSE_TI;
-extern SemVer supported_anvil_versions[17];
+extern SemVer supported_anvil_versions[18];
 
 typedef enum Canvil_Tag_Type {
     CANVIL_BASE_TAG = 0,
